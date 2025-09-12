9af06f3142ff80fffb50b55ea8519ef921591921
pepsmeister <alfredomercadoiii@gmail.com>
2025-09-12 11:02:15 +0800

    initial eslint tailwind setup

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    plugins: {
+      react,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-filename-extension': [
+        2,
+        { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+      ],
+      'no-warning-comments': [
+        1,
+        { terms: ['todo', 'fixme', 'xxx'], location: 'anywhere' },
+      ],
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      'import/prefer-default-export': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'warn',
+      'react-hooks/exhaustive-deps': 'error',
+      'no-nested-ternary': 'warn',
+      'react/require-default-props': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/jsx-key': 'warn',
+      'react/no-array-index-key': 'off',
+      'padding-line-between-statements': [
+        'warn',
+        { blankLine: 'always', prev: '*', next: 'return' },
+      ],
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+])
